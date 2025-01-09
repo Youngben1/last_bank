93cffc4ae1aaf6e274fcf22dad83770f415caa01
Youngben1 <benjaminrenny@gmail.com>
2025-01-09 11:19:24 +0100

    doughnut section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const page = () => {
-  return (
-    <div>Home Page</div>
-  )
-}
+	const loggedIn = { firstName: 'Ben' };
 
-export default page
+
+	return (
+		<section className="home">
+			<div className="home-content">
+				<header className="home-header">
+					<HeaderBox
+						type="greeting"
+						title="Welcome"
+						user={loggedIn?.firstName || "Guest"}
+						subtext="Access and manage your funds"
+					/>
+
+                    <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={3341.20}
+                    />
+				</header>
+			</div>
+		</section>
+	);
+};
+
+export default page;
