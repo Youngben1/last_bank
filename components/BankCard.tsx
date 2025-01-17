b3ace7f215c759e9b4275138dc9c2ebe4efd1472
Youngben1 <benjaminrenny@gmail.com>
2025-01-18 00:19:58 +0100

    Implement final layout

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -21,13 +21,13 @@ const BankCard = ({ account, userName, showBalance=true }:CreditCardProps) => {
                         <h1 className="text-white font-semibold text-12">{userName}</h1>
                         <h2 className="text-white font-semibold text-12">** / **</h2>
                     </div>
-                    <p className="text-14 font-semibold tracking-[1.1px] text-white">**** **** **** <span className="text-16">{account.mask}</span></p>
+                    <p className="text-14 font-semibold tracking-[1.1px] text-white">**** **** **** <span className="text-16">1234</span></p>
                 </article>
             </div>
 
             <div>
                 <Image src="/icons/Paypass.svg" alt="paypass" width={20} height={24} />
-                <Image src="/icons/mastercard.svg" alt="mastercard" width={45} height={32} />
+                <Image src="/icons/mastercard.svg" alt="mastercard" width={45} height={32} className="ml-5" />
             </div>
             <Image src="/icons/lines.png" alt="lines" width={316} height={190} className="absolute top-0 left-0" />
         </Link>
